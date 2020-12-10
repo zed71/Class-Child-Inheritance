23ad6ee82e6dcf4199c15fee76d523cef9a9e149
BrandonF <37319101+zed71@users.noreply.github.com>
2020-12-09 22:22:29 -0800

    Commit updated file

--- Create Parent Class with Children.py
+++ Create Parent Class with Children.py
@@ -14,10 +14,11 @@ class User:
             print("The password or email is incorrect.")
             
 #Child Class Employee
-            class Employee(User):
-                base_pay = 11.00
-                department = "General"
-                pin_number = "3980" 
+class Employee(User):
+    base_pay = 11.00
+    department = "General"
+    pin_number = "3980"
+                
 # This is the same method in the parent class "User".
 #The difference is that, instead of using entry password, we're using entry_pin. 
     def getLoginInfo(self):
@@ -26,9 +27,9 @@ class User:
         entry_pin = input("Enter your pin: ")
         if (entry_email == self.email and entry_pin == self.pin_number):
             print("Welcome back, {}!".format(entry_name))
-        else: print("The pin or email is incorrect") 
-#The following code invokes the methods inside each class for User and Employee.
+        else: print("The pin or email is incorrect")
         
+#The following code invokes the methods inside each class for User and Employee.
 customer = User()
 customer.getLoginInfo()
 
