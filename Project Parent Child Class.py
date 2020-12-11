51665e96180ca7c976373a9cf7a5e4a66fd7f5e6
BrandonF <37319101+zed71@users.noreply.github.com>
2020-12-10 17:54:32 -0800

    Commit change
    added two child classes

--- Project Parent Child Class.py
+++ Project Parent Child Class.py
@@ -28,6 +28,20 @@ class Writer(Book):
         else:
             print("That's not the book.")
 
+class Genre(Book):
+    age_group = "adult"
+    series = "star wars"
+
+
+    def getInformation(self):
+        genre = input("What is the genre of the book: ")
+        publisher = input("Who published the book: ")
+        copyright_month_date = input("Enter the month and date of copyright: ")
+        if (publisher == name.publisher and copyright_date == name.copyright_month_date):
+            print("Here is the book genre, {}!".format(genre))
+        else:
+            print("That's not the book.")
+
        
 #The following code invokes the methods inside each class for Book and Writer.
         
